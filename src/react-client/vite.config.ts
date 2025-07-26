928411b6d009f6b0ecec1c7cc786e277c330df56
Goran <goran@email.com>
2025-07-26 14:38:34 +0200

    add react client

---
+++ src/react-client/vite.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite'
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { resolve } from 'node:path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    viteReact(),
+    tailwindcss(),
+  ],
+  // test: {
+  //   globals: true,
+  //   environment: 'jsdom',
+  // },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('error', err);
+          });
+        }
+      }
+    }
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: false
+  }
+})
