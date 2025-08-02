ca7c5e211bdba903bff695b51fa25b65b179c56d
Goran <goran@email.com>
2025-08-02 10:03:22 +0200

    refactor: ignore generated files in prettier and update quote styles in route tree

--- src/react-client/prettier.config.js
+++ src/react-client/prettier.config.js
@@ -29,7 +29,6 @@ const config = {
   ],
   importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
   importOrderTypeScriptVersion: "5.0.0",
-  ignorePatterns: ["src/routeTree.gen.ts"],
 }
 
 export default config
