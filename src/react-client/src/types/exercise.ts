f5d3ebaa579a0975bea65cd8a17a6ead5b5233dd
Goran <goran@email.com>
2025-08-21 17:29:40 +0200

    feat: add types for exercises and

---
+++ src/react-client/src/types/exercise.ts
@@ -0,0 +1,24 @@
+export interface Exercise {
+  id: string;
+  name: string;
+  primaryMuscles: string[];
+  secondaryMuscles: string[];
+}
+
+export interface PaginatedResponse<T> {
+  items: T[];
+  totalCount: number;
+  pageSize: number;
+  pageNumber: number;
+  totalPages: number;
+  hasPreviousPage: boolean;
+  hasNextPage: boolean;
+}
+
+export type ExercisesResponse = PaginatedResponse<Exercise>;
+
+export interface ExercisesQueryParams {
+  q?: string;
+  page?: number;
+  pageSize?: number;
+}
